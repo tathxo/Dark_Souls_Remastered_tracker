2de35f29dcbdf15d12cd7c25373cf7f2b11e4525
tathxo <200347824+tathxo@users.noreply.github.com>
2026-09-04 01:09:16 -0500

    Fix apostrophes breaking map name detection for foldered autotab
    e.g. "Sen" and "s Fortress", "Duke" and "s archives"

--- scripts/autotracking/archipelago.lua
+++ scripts/autotracking/archipelago.lua
@@ -215,7 +215,7 @@ function OnNotify(key, value, old_value)
         if Tracker:FindObjectForCode("setting_auto_tab").Active == true then
             if MapIDToTab[value] then
                 for _, room in ipairs(MapIDToTab[value]) do
-                    for substr in string.gmatch(room, "[%a%s]+") do
+                    for substr in string.gmatch(room, "[%a%s']+") do
                         Tracker:UiHint("ActivateTab", substr)
                     end
                 end
